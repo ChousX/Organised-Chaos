b3d9dc8e2fc576c610a865ef7fc67759bdda0f4a
ChousX <gmanenergy@gmail.com>
2021-03-23 13:50:51 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+var m_x;
+var m_y;
+
+window.addEventListener('mousemove', function(event){
+    m_x = event.x;
+    m_y = event.y;
+    circle(m_x, m_y, 10);
+    console.log(m_x, m_y);
+});
+
+function circle(x, y, r) {
+c.beginPath()
+c.arc(x, y, r, 0, Math.PI * 2, false)
+c.fill()
+}
